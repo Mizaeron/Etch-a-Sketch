1918aec5d38ff5b65eb60fbfe63e6774b8ddc8c4
Miza <mizaeron@gmail.com>
2024-08-02 17:06:19 +0100

    add mouse events that change color

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.log("Hello World")
 
 const container = document.querySelector(".container");
-const grid = document.querySelector(".grid");
-
 
 for (let i = 0; i < 16; i++) {
 
@@ -10,4 +8,17 @@ const div = document.createElement("div");
 div.classList.add("grid");
 container.appendChild(div);
 
+}
+
+const grid = document.querySelector(".grid");
+grid.addEventListener("mousemove", hover);
+grid.addEventListener("mouseleave", leave);
+
+function hover(e) {
+    grid.style.backgroundColor = "grey";
+}
+
+function leave(e) {
+    grid.style.backgroundColor = "aqua";
+    console.log(e.type)
 }
