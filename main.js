a8d8851622011fe0c2772bd27f35d05a5a56a900
Miza <mizaeron@gmail.com>
2024-08-04 19:11:16 +0100

    create a grid using calc method which uses up % of space

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ console.log("Hello World")
 
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 10 * 10; i++) {
 
 const div = document.createElement("div");
 div.classList.add("grid");
 container.appendChild(div);
-
 }
 
 const grid = document.querySelectorAll(".grid");
 
 grid.forEach(box => {
         box.addEventListener("mouseenter", hover => box.style.backgroundColor = "grey");
-        //box.addEventListener("mouseleave", leave => box.style.backgroundColor = "aqua");
     });
