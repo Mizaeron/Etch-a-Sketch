d2c793169a32701d05d33550e176d9fddff76b99
Miza <mizaeron@gmail.com>
2024-08-06 15:19:56 +0100

    add event listener to new squares

--- main.js
+++ main.js
@@ -16,23 +16,26 @@ grid.forEach(box => {
 let reButton = document.querySelector("#reset").addEventListener("click", buttonClick);
 
 function buttonClick(e) {
-    //let entry = prompt("Enter grid number");
-    //let number = +entry;
+    let entry = prompt("Enter grid number");
+    let number = +entry;
     container.remove();
 
     const newContainer = document.createElement("div");
     newContainer.classList.add("new-container");
     const main = document.querySelector("#main");
     main.insertBefore(newContainer, main.children[1]);
-
-    let x = 12;
     
-    for (let i = 0; i < x * x; i++) {
+    for (let i = 0; i < number * number; i++) {
         const div = document.createElement("div");
         div.classList.add("newGrid");
-        div.setAttribute("style",`width: calc(100% / ${x}); height: calc(100% / ${x}); border: solid 1px;`);
+        div.setAttribute("style",`width: calc(100% / ${number}); height: calc(100% / ${number}); border: solid 1px;`);
         newContainer.appendChild(div);
-        
     }
-}
 
+let newGrid = document.querySelectorAll(".newGrid");
+
+newGrid.forEach(box => {
+    box.addEventListener("mouseenter", hover => box.style.backgroundColor = "grey");
+});
+
+}
